c07735bfc5b3d36d4599d72d6af7831ef5d25790
Dearvee <dearvee@outlook.com>
2017-05-28 20:25:49 +0800

    添加a-g音调，并添加用户输入旋律播放

--- piano1.0/index.php
+++ piano1.0/index.php
@@ -18,7 +18,7 @@ $score=implode("*",$scores);
             background: url("../../index/back.png");
         }
         .note{
-            width: 450px;
+            width: 650px;
             color: #3366CC;
             margin: 0 auto;
             margin-top: 100px;
@@ -31,7 +31,6 @@ $score=implode("*",$scores);
             text-align: center;
             line-height: 100px;
             margin: 5px;
-            box-shadow: 2px 2px 1px #3366CC;
         }
         .ctrl{
             width: 600px;
@@ -41,31 +40,20 @@ $score=implode("*",$scores);
             text-align: center;
             line-height: 40px;
         }
-        .play{
+        .ctrl li{
             display: inline-block;
+            width: 50px;
             background: #eeeeee;
-            color: #3366CC;
             margin: 10px;
             border-radius: 5px;
-            padding: 2px;
-            border: none;
-            cursor: pointer;
-            box-shadow: 2px 2px 1px #3366CC;
-            transition: all 0.3s;
+            transition: all 0.8s;
         }
-        .play:hover{
+        .ctrl li:hover{
             color: #ffffff;
             background: #3366CC;
         }
         .animation{
             animation: playMp3 0.8s;
-        }
-        .userMusic{
-            width: 300px;
-            height: 100px;
-            color: #3366CC;
-            font-size: 1.5em;
-            padding: 10px;
         }
         @keyframes playMp3 {
             0%{height: 100px;}
@@ -78,46 +66,40 @@ $score=implode("*",$scores);
         window=onload=function () {
             let scores = "<?=$score?>".split("*");
             let score=[];
-            score['F大调快板(Mozart)'] = scores[0];
-            score['粉刷匠'] = scores[1];
-            score['欢乐颂'] = scores[2];
-            $("#playScore").bind("click",function () {//播放选择音谱
-                scorePlay(score[selectItem(1)],selectItem(0),parseInt(selectItem(2)),parseInt(selectItem(3)));
+            score['粉刷匠'] = scores[0];
+            score['欢乐颂'] = scores[1];
+            $("#PlayScore").bind("click",function () {
+                    scorePlay(score[selectItem(0)],parseInt(selectItem(1)),parseInt(selectItem(2)));
                 });
-
-            $("#playUserMusic").bind("click",function () {//播放输入音谱
-                let score=$("#userMusic").val();
-                scorePlay(score,selectItem(0),parseInt(selectItem(2)),parseInt(selectItem(3)));
-            });
         }
 
         function selectItem(item) {
             let select=document.getElementsByTagName("select")[item];
             let index = select.selectedIndex;
             return select.options[index].text;
+
         }
 
-        function scorePlay(score,tune,v,d) {
+        function scorePlay(score,v,d) {
             let reg=/ *\d/g;
             let notes = score.match(reg);
             console.log(notes);
             let time=0;
             for (let i=0;i<notes.length;i++) {
                 time+=(v*(notes[i].length-1));
-                setTimeout("playMp3(" + (parseInt(notes[i][notes[i].length-1])+d).toString() + ",'"+ tune + "')",time);
+                setTimeout("playMp3(" + (parseInt(notes[i][notes[i].length-1])+d).toString() + ")",time);
             }
         }
 
-        function freePlay(tune) {
+        function freePlay() {
             let events = event.srcElement.innerText;
-            playMp3(events,tune);
+            playMp3(events);
         }
 
-        function playMp3(events,tune) {
+        function playMp3(events) {
             let mp3 = document.getElementsByTagName("audio")[events-1];
-            mp3.src="mp3/"+tune+events+"!.mp3";
-            mp3.currentTime = 0;
-            mp3.play();
+                mp3.currentTime = 0;
+                mp3.play();
             let li = document.getElementsByTagName("li");
             $(li[events-1]).removeClass().addClass("animation");
                     setTimeout(function(){
@@ -127,65 +109,41 @@ $score=implode("*",$scores);
     </script>
 </head>
 <body>
-<ul class="note" onmouseover="freePlay(selectItem(0));">
-    <li>1<audio></audio></li>
-    <li>2<audio></audio></li>
-    <li>3<audio></audio></li>
-    <li>4<audio></audio></li>
-    <li>5<audio></audio></li>
-    <li>6<audio></audio></li>
-    <li>7<audio></audio></li>
+<ul class="note" onmouseover="freePlay();">
+    <li id="btn">1<audio id="1" src="mp3/1.mp3"></audio></li>
+    <li>2<audio src="mp3/2.mp3"></audio></li>
+    <li>3<audio src="mp3/3.mp3"></audio></li>
+    <li>4<audio src="mp3/4.mp3"></audio></li>
+    <li>5<audio src="mp3/5.mp3"></audio></li>
+    <li>6<audio src="mp3/6.mp3"></audio></li>
+    <li>7<audio src="mp3/7.mp3"></audio></li>
+    <li>8<audio src="mp3/8.mp3"></audio></li>
+    <li>9<audio src="mp3/9.mp3"></audio></li>
 </ul>
 <ul class="ctrl">
-    <label>音调:</label>
-    <select>
-        <option>a</option>
-        <option>b</option>
-        <option>c</option>
-        <option>d</option>
-        <option selected="selected">e</option>
-        <option>f</option>
-        <option>g</option>
-    </select>
     <label>曲目:</label>
     <select>
-        <option>F大调快板(Mozart)</option>
         <option>粉刷匠</option>
         <option>欢乐颂</option>
     </select>
     <label>速度:</label>
     <select>
         <option>150</option>
-        <option>200</option>
+        <option selected = "selected" >200</option>
         <option>250</option>
         <option>300</option>
     </select>
-    <label>升调:</label>
+    <label>音调:</label>
     <select>
         <option>0</option>
         <option>1</option>
         <option>2</option>
         <option>3</option>
-        <option>4</option>
+        <option selected = "selected" >4</option>
     </select>
-    <li class="play">
-        <span id="playScore">Play the Score</span>
-    </li>
-    <div>数字代表音符，空格代表间隔，例如：5 3 5 3 5 3 1</div>
-    <textarea id="userMusic" class="userMusic" placeholder="你的旋律"></textarea>
-    <br/>
-    <div id="playUserMusic" class="play">Play your music!</div>
-</ul>
-<ul style="color: #3366CC;">
-    <h2>About</h2>
     <li>
-        <a href="piano1.0">上一个版本</a>
-    </li>
-    <li>power by
-        <a href="http://www.dearvee.com">vee</a>
+        <span id="PlayScore">Play</span>
     </li>
 </ul>
 </body>
-<script>
-</script>
 </html>
